f05a62727766842d4393e689f2a985c67d1e3dd9
yasmim_fariaa <yasmimstefanefaria@gmail.com>
2026-05-14 09:31:27 -0300

    alterando a main

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import servidor.App.servidor_app
 import socket
 from servidor.App.servidor_app import servidor_app
-
 if __name__ == '__main__':
-
     servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     servidor.bind(("localhost", 12381))
     servidor.listen(5)
 
+    print("Aguardando conexão...")
     conn, addr = servidor.accept()
-    cliente = servidor_app(sock=conn)  # passa o socket já existente
-    dados = cliente.receive(1024)
-    cliente.send(b"resposta", 1024)
+    print(f"Conectado a {addr}")
+
+    cliente = servidor_app(sock=conn)
+    msg = b"Ola! Conexao estabelecida."
+    cliente.send(msg, len(msg))
+
+
+    conn.close()
+    servidor.close()
