10ff8259c019804bade425b8f3d0b7cef7e92687
carlos <carloslopesassis008@gmail.com>
2026-05-14 09:35:50 -0300

    dev: teste-conect-main servidor_app 2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from reportlab.lib.pagesizes import legal
+
 import servidor.App.servidor_app
 import socket
 from servidor.App.servidor_app import servidor_app
@@ -5,10 +7,11 @@ from servidor.App.servidor_app import servidor_app
 if __name__ == '__main__':
 
     servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    servidor.bind(("0.0.0.0", 8080))
+    servidor.bind(('localhost', 12381))
     servidor.listen(5)
-
+    msg = b"resposta do servidor"
     conn, addr = servidor.accept()
     cliente = servidor_app(sock=conn)  # passa o socket já existente
-    dados = cliente.receive(1024)
-    cliente.send(b"resposta", 1024)
+    dados = cliente.receive(8)
+    cliente.send(msg, len(msg))
+    print(dados)
